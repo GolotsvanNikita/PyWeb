3ee45d46070f364f796f5c23a7e699c52a57cd33
Nikita Holotsvan <hohoko555888@gmail.com>
2025-11-04 00:50:01 +0200

    added more static files

--- PyWebP33/Project33/App33/static/js/site.js
+++ PyWebP33/Project33/App33/static/js/site.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', () =>
 {
     console.log('Script works');
 });
+
+const btn = document.getElementById('click');
+const audio = new Audio(btn.getAttribute('data-sound'));
+audio.preload = 'auto';
+
+btn.addEventListener('mouseenter', () =>
+{
+    audio.currentTime = 0;
+    audio.play();
+});
